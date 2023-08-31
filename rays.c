4e8bfa433ae55c5fbbf3fbdd3b65ee1b617df8cb
alexbenkey <73598217+alexbenkey@users.noreply.github.com>
2023-08-31 18:59:12 +0200

    pre-pull

--- rays.c
+++ rays.c
@@ -18,42 +18,42 @@
 
 //---------------------------Draw Rays and Walls--------------------------------
 
-void	draw_line(t_map *map, float int_x, float int_y)
-{
-	minimap_wrap_print(mini_x(map, map->rays->draw_x), \
-	mini_y(map, map->rays->draw_y), map, ft_pixel(0, 0, 255, 200));
-	map->rays->draw_x += int_x;
-	map->rays->draw_y += int_y;
-	map->rays->dist_y -= int_y;
-}
+// void	draw_line(t_map *map, float int_x, float int_y)
+// {
+// 	minimap_wrap_print(mini_x(map, map->rays->draw_x), \
+// 	mini_y(map, map->rays->draw_y), map, ft_pixel(0, 0, 255, 200));
+// 	map->rays->draw_x += int_x;
+// 	map->rays->draw_y += int_y;
+// 	map->rays->dist_y -= int_y;
+// }
 
-static void	draw_ray(t_map *map)
-{
-	float	int_x;
-	float	int_y;
+// static void	draw_ray(t_map *map)
+// {
+// 	float	int_x;
+// 	float	int_y;
 
-	map->rays->draw_x = map->player.x_coor;
-	map->rays->draw_y = map->player.y_coor;
-	map->rays->dist_x = (map->rays->ray_x - map->player.x_coor);
-	map->rays->dist_y = (map->rays->ray_y - map->player.y_coor);
-	int_x = map->rays->dist_x / 1000;
-	int_y = map->rays->dist_y / 1000;
-	if (map->rays->dist_x == 0 || map->rays->dist_y == 0)
-		return ;
-	if (map->rays->dist_h <= map->rays->dist_h)
-	{
-		if (map->rays->dist_y > 0)
-		{
-			while (map->rays->dist_y > 0)
-				draw_line(map, int_x, int_y);
-		}
-		else if ((map->rays->dist_y < 0))
-		{
-			while (map->rays->dist_y < 0)
-				draw_line(map, int_x, int_y);
-		}
-	}
-}
+// 	map->rays->draw_x = map->player.x_coor;
+// 	map->rays->draw_y = map->player.y_coor;
+// 	map->rays->dist_x = (map->rays->ray_x - map->player.x_coor);
+// 	map->rays->dist_y = (map->rays->ray_y - map->player.y_coor);
+// 	int_x = map->rays->dist_x / 1000;
+// 	int_y = map->rays->dist_y / 1000;
+// 	if (map->rays->dist_x == 0 || map->rays->dist_y == 0)
+// 		return ;
+// 	if (map->rays->dist_h <= map->rays->dist_h)
+// 	{
+// 		if (map->rays->dist_y > 0)
+// 		{
+// 			while (map->rays->dist_y > 0)
+// 				draw_line(map, int_x, int_y);
+// 		}
+// 		else if ((map->rays->dist_y < 0))
+// 		{
+// 			while (map->rays->dist_y < 0)
+// 				draw_line(map, int_x, int_y);
+// 		}
+// 	}
+// }
 
 void	move_to_next_point_h(t_map *map)
 {
@@ -174,14 +174,40 @@ void rays_horizontal(t_map *map, float Tan)
 		move_to_next_point_h(map);
 }
 
+void draw_vert_line(t_map *map, int height, int width, int start_y, int pos)
+{
+	int	x;
+	int	y;
+	int start_x;
+
+	x = pos;
+	start_x = x;
+	y = start_y;
+	while (x < (start_x +width) && x < WIDTH)
+	{
+		while (y < height)
+		{
+			put_pixel_wrap(map->img, x, y, ft_pixel(0, 255, 0, 150));
+			y++;
+		}
+		y = start_y;
+		x++;
+	}
+}
+
 int	cast_rays(t_map *map)
 {
 	float	tang;
 	int		i;
+	int		lineH;
+	float	line_int;
+	float	ray_int;
 
+	ray_int = (FOV * 4)/ WIDTH;
+	line_int = (WIDTH / (FOV * 4));
 	i = 0;
 	map->rays->ray_angle = map->player.rotation + FOV;
-	while (i < (FOV * 4))
+	while (i < WIDTH)
 	{
 		tang = tan((degToRad(map->rays->ray_angle)));
 		rays_vertical(map, tang);
@@ -192,9 +218,12 @@ int	cast_rays(t_map *map)
 			map->rays->ray_y = map->rays->vert_y;
 			map->rays->dist_h = map->rays->dist_v;
 		}
-		draw_ray(map);
+		//draw_ray(map);
 		// draw beam on screen
-		map->rays->ray_angle = FixAng(map->rays->ray_angle - 0.5);
+		lineH = ((TILE_RAD * 2) * HEIGHT) / (map->rays->dist_h);
+		//if(lineH>320){ lineH=320;}
+		draw_vert_line(map, lineH, 1, 160, i);
+		map->rays->ray_angle = FixAng(map->rays->ray_angle - ray_int);
 		map->rays->dof = 0;
 		i++;
 	}
